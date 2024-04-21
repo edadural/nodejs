36f0e1e69d64333f94d88c2f1b87b6b980939c7e
Eda Dural <edaadurall@gmail.com>
2024-04-21 22:21:35 +0300

    weather-app 4

--- weather-app/web-server/src/app.js
+++ weather-app/web-server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const hbs = require("hbs")
 
 // console.log(__dirname);
 // console.log(path.join(__dirname, '../public'));
@@ -11,8 +12,11 @@ app.set('view engine', 'hbs')
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
-const viewsPath = path.join(__dirname, '../public/views')
+const viewsPath = path.join(__dirname, '../public/templates/views')
 app.set('views', viewsPath)
+
+const partialsPath = path.join(__dirname, './public/templates/partials')
+hbs.registerPartials(partialsPath)
 
 app.get('', (req, res) => {
     res.render('index', {
@@ -31,6 +35,7 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Yardım sayfası',
+        name: 'Eda',
         helpText: 'Bu bir deneme yazısıdır.'
     })
 })
@@ -56,6 +61,14 @@ app.get('/weather', (req, res) => {
 //     res.send([{ name: 'Can' },
 //     { name: 'Canan' }])
 // })
+
+app.get('*', (req, res) => {
+    res.render("404", {
+        title: "404 -Sayfa Bulunamadı",
+        name: "humeyra cimen",
+        errorMessage: "Aradığınız sayfa bulunamadı."
+    })
+})
 
 app.listen(3000, () => {
     console.log('Sunucu 300 portunu dinliyor.');
